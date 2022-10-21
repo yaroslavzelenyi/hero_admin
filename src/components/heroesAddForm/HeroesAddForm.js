570ae8eed97b681e2fa7098728828fc9b1d3a31c
Yaroslav Zelenyi <yaroslagreen@gmail.com>
2022-10-21 21:10:10 +0300

    first commit

---
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { heroCreated } from '../../actions';
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильтроваться
+// Уникальный идентификатор персонажа можно сгенерировать через uiid
+// Усложненная задача:
+// Персонаж создается и в файле json при помощи метода POST
+// Дополнительно:
+// Элементы <option></option> желательно сформировать на базе
+// данных из фильтров
+
+const HeroesAddForm = () => {
+    const [heroName, setHeroName] = useState('');
+    const [heroDescr, setHeroDescr] = useState('');
+    const [heroElement, setHeroElement] = useState('');
+
+    
+    const {filters, filtersLoadingStatus} = useSelector(state => state.filters);
+    const dispatch = useDispatch();
+    const onSubmitHandler = (e) =>{
+        e.preventDefault();
+        const newHero = {
+            id: uuidv4(),
+            name: heroName,
+            description: heroDescr,
+            element: heroElement
+        }
+        dispatch(heroCreated(newHero));
+        setHeroName('');
+        setHeroDescr('');
+        setHeroElement('');
+    }
+    return (
+        <form className="border p-4 shadow-lg rounded" onSubmit={onSubmitHandler}>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                <input 
+                    required
+                    type="text" 
+                    name="name" 
+                    className="form-control" 
+                    id="name" 
+                    onChange={(e) => setHeroName(e.target.value)}
+                    placeholder="Как меня зовут?"/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="text" className="form-label fs-4">Описание</label>
+                <textarea
+                    required
+                    name="text" 
+                    className="form-control" 
+                    id="text" 
+                    placeholder="Что я умею?"
+                    onChange={(e) => setHeroDescr(e.target.value)}
+                    style={{"height": '130px'}}/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                <select 
+                    required
+                    className="form-select" 
+                    id="element" 
+                    name="element"
+                    value={heroElement}
+                    onChange={(e) => setHeroElement(e.target.value)}>
+                    
+                    <option >Я владею элементом...</option>
+                    <option value="fire">Огонь</option>
+                    <option value="water">Вода</option>
+                    <option value="wind">Ветер</option>
+                    <option value="earth">Земля</option>
+                </select>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Создать</button>
+        </form>
+    )
+}
+
+export default HeroesAddForm;
---
+++ src/store/index.js
@@ -0,0 +1,43 @@
+
+import heroes from '../reducers/heroes';
+import {configureStore} from '@reduxjs/toolkit';
+import filters from '../reducers/filters';
+
+
+const stringMiddleware = () => (next) => (action) =>{
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type: action
+            })
+        }
+        return oldDispatch(action)
+    }
+    return store;
+}
+// const store = createStore( 
+//         combineReducers({heroes, filters}), 
+//         compose(applyMiddleware(ReduxThunk, stringMiddleware),
+//                 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//         )
+//         );
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    
+    devTools: process.env.NODE_ENV !=='pruduction'
+    
+})
+export default store;
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
